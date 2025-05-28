6a3281f9b79a39083e29410d17f35e3bc47d6ad4
wan <wan@wans-MacBook-Pro.local>
2025-05-29 04:52:19 +0800

    update

--- SR_final_v1/SR_radar/src/tdt_vision/detect/src/detect.cpp
+++ SR_final_v1/SR_radar/src/tdt_vision/detect/src/detect.cpp
@@ -235,7 +235,8 @@ void Detect::callback(const std::shared_ptr<sensor_msgs::msg::Image> msg) {
       auto safe_rect = getSafeRect(img,max_rect);
       //cv::rectangle(img,safe_rect,cv::Scalar(255,255,255),2);
 
-      car.center=cv::Point2f(max_rect.x+max_rect.width/2,max_rect.y);
+      //car.center=cv::Point2f(max_rect.x+max_rect.width/2,max_rect.y);
+      car.center=cv::Point2f(max_rect.x+max_rect.width/2,car.car.bottom);
       
       if(car.color==0){
           detect_result.blue_x[car.number-1]=car.center.x;
@@ -271,13 +272,13 @@ void Detect::callback(const std::shared_ptr<sensor_msgs::msg::Image> msg) {
   std::cout<<"Detect Time: "<<time_used.count()*1000<<"ms"<<std::endl;
   
   // 确保在所有情况下都显示图像
-  cv::Mat final_img;
-  cv::resize(img, final_img, cv::Size(1536, 1125));
-  cv::imshow("detect", final_img);
-  auto key = cv::waitKey(1);
-  if(key=='r'){
-      debug = !debug;
-  }
+  //cv::Mat final_img;
+  //cv::resize(img, final_img, cv::Size(1536, 1125));
+  //cv::imshow("detect", final_img);
+  //auto key = cv::waitKey(1);
+  //if(key=='r'){
+  //    debug = !debug;
+  //}
 }
 }// namespace tdt_radar
 RCLCPP_COMPONENTS_REGISTER_NODE(tdt_radar::Detect)
